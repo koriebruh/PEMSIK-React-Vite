4ec5deec901ed2a511c36ff1aa1e529d98a90c60
koriebruh <koriebruh@gmail.com>
2024-12-06 10:15:25 +0700

    asw

--- src/RouteList.jsx
+++ src/RouteList.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
         children:[
             {
                 path:'mahasiswa',
-                index:true,
+                // index:true,
                 element: <AdminLayout/>,
             }
         ]
